2e9400f5d6f3bc2cd84e1f3a6f008c985b173928
Heather Gaya <46654652+heathergaya@users.noreply.github.com>
2021-11-18 12:42:35 -0500

    Create Runcode.R

---
+++ Runcode.R
@@ -0,0 +1,11 @@
+dget(fake.dat, "fakedat.txt")
+library(runjags)
+
+sim_test <- run.jags("Discrete_time_cont_space_ind.txt", data = fake.dat$jags.data, 
+                     monitor = fake.dat$jags.pars, adapt = 100, n.chains = 2, sample = 200, burnin = 0, 
+                     inits =  fake.dat$jags.inits, method = "parallel")
+
+#this runs fine when line 61 is commented out. The minute it is included, the error "SimpleRange:leftoffset" appears. 
+
+tt <- as.matrix(sim_test$mcmc)
+min(tt); max(tt)
